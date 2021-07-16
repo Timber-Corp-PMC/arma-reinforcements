484598469434a2cdcaf82e4679c313d62eb68c01
Geelik <pic.thib27@gmail.com>
2021-07-16 18:16:31 +0200

    fix a missing semicolon

--- reinforcements/CfgFunctions.hpp
+++ reinforcements/CfgFunctions.hpp
@@ -1,10 +1,10 @@
 /*
-version: 1.0.0
+version: 1.0.1
 */
 class TimberCorpReinforcements {
     class Main {
         file = "reinforcements\functions";
         class callReinforcement{};
         class registerReinforcement{};
-    }
+    };
 };
